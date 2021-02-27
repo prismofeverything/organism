f21139bd724ecc6ddf6cc0524dfd97d2ebba3fee
Ryan Spangler <ryan.spangler@gmail.com>
2021-02-26 23:17:52 -0800

    circulate

--- test/clj/organism/organism_test.clj
+++ test/clj/organism/organism_test.clj
@@ -264,7 +264,8 @@
                  game
                  [0 0 0 0
                   1 1 0 0 0 1
-                  2 0 1 1])]
+                  2 0 1 1
+                  1 1 1 1])]
       (println)
       (println)
       (println)
